a6d5e9291cdb3b6c5229450b7c9a183027758a3b
Vinicius Agostini <vinicius.agostini19@gmail.com>
2022-12-05 20:00:54 -0300

    feat: implement Match type

---
+++ ms-consolidation/internal/domain/entity/match.go
@@ -0,0 +1,28 @@
+package entity
+
+import (
+	"time"
+)
+
+type Match struct {
+	ID      string
+	TeamA   *Team
+	TeamB   *Team
+	TeamAID string
+	TeamBID string
+	Date    time.Time
+	Status  string
+	Result  MatchResult
+	Actions []GameAction
+}
+
+func NewMatch(id string, teamA *Team, teamB *Team, date time.Time) *Match {
+	return &Match{
+		ID:      id,
+		TeamA:   teamA,
+		TeamB:   teamB,
+		TeamAID: teamA.ID,
+		TeamBID: teamB.ID,
+		Date:    date,
+	}
+}
